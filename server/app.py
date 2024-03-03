5d0a9c806a7d550ea3d730dadcde23c84de38d5c
ussenuk <ukimanuka@gmail.com>
2024-03-03 14:08:16 +0300

    worked on the routing and views lab in order to understand the routing mechanisms

--- server/app.py
+++ server/app.py
@@ -4,5 +4,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter, end="\n")
+    return f'{parameter}'
+
+@app.route('/count/<int:number>')
+def count(number):
+    l = [n for n in range(number)] 
+    # for i in l:
+    #     print(i, end="\n")
+    #     # i
+    w = '\n'.join(map(str,l))
+    return f'{w}\n'
+
+@app.route('/math/<int:num1>/<operator>/<int:num2>')
+def math(num1, operator, num2):
+    if operator == '+':
+        return f'{num1 + num2}'
+    if operator == '-':
+        return f'{num1 - num2}'
+    if operator == '*':
+        return f'{num1 * num2}'
+    if operator == 'div':
+        return f'{num1 / num2}'
+    if operator == '%':
+        return f'{num1 % num2}'
+    
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
